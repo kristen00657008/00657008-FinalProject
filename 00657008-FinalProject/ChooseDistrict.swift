06fb241357e3635ce2c5b4b558536256575d9101
tony <qq1456978523@gmail.com>
2019-12-17 20:40:13 +0800

    2019.12.17 action sheet contextMenu

--- 00657008-FinalProject/ChooseDistrict.swift
+++ 00657008-FinalProject/ChooseDistrict.swift
@@ -9,13 +9,23 @@
 import SwiftUI
 
 struct ChooseDistrict: View {
+    @Binding var selectedIndex:Int
+    var districts = ["中正區","大同區","中山區","松山區","大安區","萬華區","信義區","士林區","北投區","內湖區","南港區","文山區"]
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello World!"/*@END_MENU_TOKEN@*/)
+        
+        Section{
+            Picker(selection: self.$selectedIndex, label: Text("")){
+                ForEach(0..<self.districts.count) { (index) in
+                    Text(self.districts[index])
+                }
+            }
+        }
     }
 }
 
 struct ChooseDistrict_Previews: PreviewProvider {
     static var previews: some View {
-        ChooseDistrict()
+        ChooseDistrict(selectedIndex: .constant(0))
     }
 }
--- 00657008-FinalProject/ShopList.swift
+++ 00657008-FinalProject/ShopList.swift
@@ -12,7 +12,7 @@ struct ShopList: View {
     
     @State private var shops = [Shop]()
     @State private var selectedIndex = 0
-    @State private var currentDistrict = "選擇區域"
+    @State private var currentDistrict = ""
     @State private var showChooseDistrict = false
     var districts = ["中正區","大同區","中山區","松山區","大安區","萬華區","信義區","士林區","北投區","內湖區","南港區","文山區"]
     
@@ -27,8 +27,23 @@ struct ShopList: View {
             }.resume()
             
         }
-        
     }
+    
+    private var sheet: ActionSheet {
+        
+        let action = ActionSheet(
+            title: Text("是否需要Wifi").font(Font.system(size: 30)),
+            message: Text("是否需要Wifi"),
+            buttons: [
+                .default(Text("需要"), action: { print("hello") }),
+                .destructive(Text("不需要"), action: { print("hello") }),
+                .cancel(Text("不重要"), action: { print("hello") }),
+            ]
+        )
+        
+        return action
+    }
+    
     var body: some View {
         
         NavigationView{
@@ -41,21 +56,31 @@ struct ShopList: View {
             //.navigationBarTitle("台北市咖啡廳一覽表")
             .navigationBarItems(leading:
                 HStack{
+                    VStack{
+                        Text("選擇區域")
+                            .bold()
+                            .foregroundColor(Color.blue)
+                            .contextMenu {
+                                ForEach(0..<self.districts.count) { (index) in
+                                    Button(action: {
+                                        self.currentDistrict = String(self.districts[index])
+                                    }) {Text(self.districts[index]) }
+                                }
+                            
+                        }
+                        Text(currentDistrict)
+                    }
+                    .offset(x: 0, y: 8)
+                },trailing:
+                HStack{
                     Button(action: {
                         self.showChooseDistrict = true
-                    }) {
-                        Text("選擇區域")
-                    }
-                    .sheet(isPresented: $showChooseDistrict) {
-                        Picker(selection: self.$selectedIndex, label: Text("")){
-                            ForEach(0..<self.districts.count) { (index) in
-                               Text(self.districts[index])
-                            }
+                    }) {Image("Wifi")
+                            .renderingMode(.original)
+                            .resizable()
+                            .frame(width: 30, height: 30)
                         }
-                    }
-                    
-                    
-                    
+                        .actionSheet(isPresented: $showChooseDistrict, content: {sheet})
                 }
             )
         }
